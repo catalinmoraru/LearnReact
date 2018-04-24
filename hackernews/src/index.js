54016121b8d29fc07bde8b5e0f05e7f3aaeafae2
catalin.moraru <catalin.moraru@visma.com>
2018-04-24 16:30:42 +0300

    first commit

---
+++ hackernews/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render( <App /> , document.getElementById('root'));
+ ReactDOM.render(<h1>Hello React World</h1> , document.getElementById('root1'));
+
+if (module.hot) {
+	module.hot.accept();
+}
+
+registerServiceWorker();
+
+
