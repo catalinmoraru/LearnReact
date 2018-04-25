120bfbed53fa892543bce24faef76349a0634039
catalin.moraru <catalin.moraru@visma.com>
2018-04-25 14:45:56 +0300

    road-to-react-chapter2 split into components

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 
@@ -18,58 +17,88 @@ const list = [
 		num_comments: 2,
 		points: 5,
 		objectID: 1,
-	}, ];
+	},];
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-class Developer {
-	constructor(firstname, lastname) {
-	this.firstname = firstname;
-	this.lastname = lastname; }
-	getName() {
-		return this.firstname + ' ' + this.lastname;
+class Search extends Component {
+	render() {
+		const {value, onChange} = this.props;
+		return (
+			<form>
+				<input
+					type="text"
+					value={value}
+					onChange={onChange}
+				/></form>
+		);
+	}
+}
+
+class Table extends Component {
+	render() {
+		const {list, pattern, onDismiss} = this.props;
+		return (
+			<div>
+				{list.filter(isSearched(pattern)).map(item =>
+					<div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+						<span>{item.author}</span>
+						<span>{item.num_comments}</span>
+						<span>{item.points}</span>
+						<span>
+              <button
+				  onClick={() => onDismiss(item.objectID)}
+				  type="button"
+			  > Dismiss
+              </button>
+            </span>
+					</div>)}
+			</div>);
 	}
 }
 
 class App extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			list,
+			searchTerm: ''
+		};
+
+		this.onDismiss = this.onDismiss.bind(this);
+		this.onSearchChange = this.onSearchChange.bind(this);
+	}
+
+	onDismiss(id) {
+		const updatedList = this.state.list.filter(item => item.objectID !== id);
+		this.setState({list: updatedList});
+	}
+
+	onSearchChange(event) {
+		this.setState({searchTerm: event.target.value});
+	}
+
+
 	render() {
-		const robin = new Developer('Robin', 'Wieruch');
-		console.log(robin.getName());
-	    let firstName = "a s Gigel ce mai mare";
-	    let lastName = "Haiduc";
-		const helloWorld = 'Welcome to the Road to learn React';
+		const {list, searchTerm} = this.state;
+
 		return (
 			<div className="App">
-				<h2>Hello {firstName}  + {lastName}</h2>
-				<h2>{helloWorld}</h2>
-
-				{list.map(function(item) {
-					return (
-						<div key={item.objectID}>
-							<span><a href={item.url}>{item.title}</a></span>
-							<span>{item.author}</span>
-							<span>{item.num_comments}</span>
-							<span>{item.points}</span>
-						</div>
-					);
-				})}
+				<Search
+					value={searchTerm}
+					onChange={this.onSearchChange}/>
+				<Table
+					list={list}
+					pattern={searchTerm}
+					onDismiss={this.onDismiss}/>
 			</div>
 		);
 
-	} }
+	}
+}
 
 export default App;
